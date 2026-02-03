af09d8255f333d47170b258d6d0fd488939981f7
Ruben Hernandez <Rubenhnt@gmail.com>
2026-02-03 18:15:36 +0000

    setting up tabs

---
+++ app/tabs/index.tsx
@@ -0,0 +1,28 @@
+import { Text, StyleSheet, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  content: {
+    fontSize: 48
+  }
+})
