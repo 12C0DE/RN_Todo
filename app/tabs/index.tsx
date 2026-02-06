10c3e67170edd8e642a8db6417663849f85d4bce
Ruben Hernandez <Rubenhnt@gmail.com>
2026-02-06 01:23:28 +0000

    testing out using convex

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, TouchableOpacity, useColorScheme, View } from "react-native";
+import { useMutation, useQuery } from "convex/react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { api } from "../../convex/_generated/api";
 import useTheme from "../hooks/useTheme";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+
+  const todos = useQuery(api.todos.getTodos);
+  console.log(todos);
+
+  const addTodo = useMutation(api.todos.addTodo);
+  const clearTodos = useMutation(api.todos.clearAllTodos);
+
   return (
     <View style={styles.container}>
       <Text style={styles.content}>
         Edit app/index.tsx to edit this screen.
       </Text>
-      <TouchableOpacity onPress={() => toggleDarkMode()}>Toggle Theme</TouchableOpacity>
+      <TouchableOpacity onPress={() => toggleDarkMode()}>
+        Toggle Theme
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({ text: "walking" })}>
+        Add Todo
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => clearTodos()}>
+        Clear Todos
+      </TouchableOpacity>
     </View>
   );
 }
